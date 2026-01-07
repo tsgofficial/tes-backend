efbc49f8e1441c8d4f5b2b43e91debc3cd46eaf3
tsgofficial <baihguibno@gmail.com>
2026-01-07 21:54:50 +0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,41 +1,41 @@
 const dotenv = require('dotenv');
 const path = require('path');
-const Joi = require('joi');
+// const Joi = require('joi');
 // const logger = require('./logger');
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-const envVarsSchema = Joi.object()
-  .keys({
-    NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
-    PORT: Joi.number().default(3000),
-    MYSQL_HOST: Joi.string().required().description('MySQL database host'),
-    MYSQL_PORT: Joi.number().default(3306).description('MySQL database port'),
-    MYSQL_DB: Joi.string().required().description('MySQL database name'),
-    MYSQL_USER: Joi.string().required().description('MySQL database username'),
-    MYSQL_PASSWORD: Joi.string().required().description('MySQL database password'),
-    JWT_SECRET: Joi.string().required().description('JWT secret key'),
-    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
-    JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
-    JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
-      .default(10)
-      .description('minutes after which reset password token expires'),
-    JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
-      .default(10)
-      .description('minutes after which verify email token expires'),
-    SMTP_HOST: Joi.string().description('server that will send the emails'),
-    SMTP_PORT: Joi.number().description('port to connect to the email server'),
-    SMTP_USERNAME: Joi.string().description('username for email server'),
-    SMTP_PASSWORD: Joi.string().description('password for email server'),
-    EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
-  })
-  .unknown();
+// const envVarsSchema = Joi.object()
+//   .keys({
+//     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+//     PORT: Joi.number().default(3000),
+//     MYSQL_HOST: Joi.string().required().description('MySQL database host'),
+//     MYSQL_PORT: Joi.number().default(3306).description('MySQL database port'),
+//     MYSQL_DB: Joi.string().required().description('MySQL database name'),
+//     MYSQL_USER: Joi.string().required().description('MySQL database username'),
+//     MYSQL_PASSWORD: Joi.string().required().description('MySQL database password'),
+//     JWT_SECRET: Joi.string().required().description('JWT secret key'),
+//     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
+//     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
+//     JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
+//       .default(10)
+//       .description('minutes after which reset password token expires'),
+//     JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
+//       .default(10)
+//       .description('minutes after which verify email token expires'),
+//     SMTP_HOST: Joi.string().description('server that will send the emails'),
+//     SMTP_PORT: Joi.number().description('port to connect to the email server'),
+//     SMTP_USERNAME: Joi.string().description('username for email server'),
+//     SMTP_PASSWORD: Joi.string().description('password for email server'),
+//     EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
+//   })
+//   .unknown();
 
-const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+// const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
 
-if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
-}
+// if (error) {
+//   throw new Error(`Config validation error: ${error.message}`);
+// }
 
 const config = {
   env: 'production',
@@ -51,22 +51,11 @@ const config = {
     logging: false,
   },
   jwt: {
-    secret: envVars.JWT_SECRET,
-    accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
-    refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
-    resetPasswordExpirationMinutes: envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
-    verifyEmailExpirationMinutes: envVars.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES,
-  },
-  email: {
-    smtp: {
-      host: envVars.SMTP_HOST,
-      port: envVars.SMTP_PORT,
-      auth: {
-        user: envVars.SMTP_USERNAME,
-        pass: envVars.SMTP_PASSWORD,
-      },
-    },
-    from: envVars.EMAIL_FROM,
+    secret: '4669a8beb52933edc5d69825f68b5eaf1fc141c889894800d45b1aaa3ee36760',
+    accessExpirationMinutes: 30,
+    refreshExpirationDays: 30,
+    resetPasswordExpirationMinutes: 10,
+    verifyEmailExpirationMinutes: 10,
   },
 };
 module.exports = config;
