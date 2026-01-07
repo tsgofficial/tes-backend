81399d951d24b19295f3da55c7e2859d1fe67191
tsgofficial <baihguibno@gmail.com>
2026-01-07 22:02:24 +0800

    ab

--- src/config/config.js
+++ src/config/config.js
@@ -42,7 +42,7 @@ const config = {
   port: '3000',
   mysql: {
     host: 'localhost',
-    port: '3306',
+    port: 3306,
     database: 'tmoiltr1_tes',
     username: 'tmoiltr1_tes',
     password: 'tmoiltrans123456',
